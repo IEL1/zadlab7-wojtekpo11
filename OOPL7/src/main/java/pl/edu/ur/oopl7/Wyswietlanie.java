0571a56ae5c6db6f500fb4f6764b9c7c69f1ff80
Wojciech <Wojciech@DESKTOP-LHA1MAK>
2018-05-06 18:29:12 +0200

    Wyswietlanie

---
+++ OOPL7/src/main/java/pl/edu/ur/oopl7/Wyswietlanie.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl7;
+
+/**
+ *
+ * @author Wojciech
+ */
+public class Wyswietlanie {
+
+    private static void as() {
+        Test testClass = new Test();
+        testClass.test2D();
+        testClass.test3D();
+        testClass.tablica2D();
+        testClass.tablica3D();
+        testClass.testCO();
+    }
+
+    public static void main(String[] args) {
+        as();
+    }
+}
